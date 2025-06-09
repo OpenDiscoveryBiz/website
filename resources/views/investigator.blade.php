6c2c1a077f47992e535a5aa1ad9558a6b45d2338
Casper Biering <cb@peercraft.com>
2025-06-09 20:47:04 +0000

    update text

--- resources/views/investigator.blade.php
+++ resources/views/investigator.blade.php
@@ -11,7 +11,16 @@
 
     <p>The Business Investigator displays an icon next to the address bar. When clicking the icon a popup appears with selected relevant information about the website owner. This includes parts of the public records of the business entity as well as relevant selfasserted and third party attested claims.</p>
 
-    <p>The current prerelease version is only available for the Google Chrome browser. Future versions will also be available for the Mozilla Firefox and Microsoft Edge browsers.</p>
+    <p>Business Investigator is available in Google's <a href="https://chrome.google.com/webstore/detail/business-investigator/kdnkelhohlogiebdcmicchkadjmkhdgm" target="_blank">Chrome webstore</a> and works with most Chrome based browsers.</p>
 
-    <p><a href="https://chrome.google.com/webstore/detail/business-investigator/kdnkelhohlogiebdcmicchkadjmkhdgm" target="_blank">Get the prerelease version in the Chrome webstore!</a></p>
+    <p style="margin-top: 3em"><b>PRIVACY POLICY</b></p>
+
+    <p>The purpose of the Business Investigator extension and the associated Resolver is exclusively to provide the user with authoritative and trustworthy information about the Business entity running the website - or the specific page of a website - that the user is visiting.</p>
+
+    <p>In order to verify the binding between the website/webpage and the business entity, the extension needs to get the domain or page URL to look for a specific tag/header/file containing the asserted internationalized Business Registry ID.</p>
+
+    <p>The Business ID is sent to the Resolver in order to retrieve information from the Business Registry and optionally more trustworthy sources, and then locally compare if the business is asociated with the website.</p>
+
+    <p>Business entity data may be cached by the resolver for up to 24 hours to avoid excessive load on the data sources. They will NOT be used to compile any kind of web usage history for any specific users.</p>
+
 @stop
